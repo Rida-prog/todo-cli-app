822e1d41d5f6daa01425d393cfcaf49a7a8e8306
Rida Fatima <ridafatimanasir1@gmail.com>
2025-11-27 07:56:02 +0000

    naming fuctions

--- .practice.node.js/app.js
+++ .practice.node.js/app.js
@@ -1,10 +1,10 @@
-const populateProducts = async () => {
+async function populateProducts() {
     const products = document.querySelector('#products')
     products.innerHTML = ''
     const data = [
-        {id: 'A1', name: 'Vaccum Cleaner', rrp: '99.99', info: 'the worst vaccum in the world.'},
-        {id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.'}
-        {id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.'}
+        { id: 'A1', name: 'Vaccum Cleaner', rrp: '99.99', info: 'the worst vaccum in the world.' },
+        { id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.' },
+        { id: 'B1', name: 'Chocolate Bar', rrp: '22.40', info: 'Delicious overpriced chocolate.' }
     ]
     for (const product of data) {
         const item = document.createElement('product-item')
